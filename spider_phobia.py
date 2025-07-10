e53d023cea5e16b1fe3447be0ee093ed4c955e70
laur290 <mihai.stanisteanu@stud.fim.upb.ro>
2025-07-10 13:53:36 +0200

    Updated code (extraction+neurokit2 processing)

--- spider_phobia.py
+++ spider_phobia.py
@@ -2,7 +2,7 @@ import os
 from sklearn.cluster import KMeans
 import neurokit2 as nk
 import pandas as pd
-
+import matplotlib.pyplot as plt
 #defining functions that extract the data from each type of txt file into a data frame
 
 def load_deflection(path):
@@ -35,8 +35,9 @@ def load_conductance(path):
     df["Timestamp"] = pd.to_datetime(df["Timestamp"]) # clearer time moments
     return df
     
+# data extraction
 
-root_folder="D:\erasmus\work"
+root_folder=r"D:\erasmus\work"
 patient_data = {}
 
 for patient_folder in os.listdir(root_folder):
@@ -61,4 +62,36 @@ for patient_folder in os.listdir(root_folder):
     gsr_file = os.path.join(FOLDER_PATH, "BitalinoGSR.txt")
     if os.path.isfile(gsr_file):
         patient_data[patient_folder]["gsr"] = load_conductance(gsr_file)
+        
+# data processing using neurokit2
+
+processed_data={}
+
+for patient_folder in patient_data:
+    processed_data[patient_folder]={}
+    
+    #breath rate
+    signal1=patient_data[patient_folder]["breath"]
+    if signal1 is not None:
+        rsp_signals, rsp_info = nk.rsp_process(signal1["Deflection"],
+                                               sampling_rate=100)
+        processed_data[patient_folder]["breath"]={"signals": rsp_signals, "info":rsp_info}
+     
+    #ECG
+    signal2=patient_data[patient_folder]["ecg"]
+    if signal2 is not None:
+        ecg_signals, ecg_info=nk.ecg_process(signal2["Voltage"],
+                                             sampling_rate=100)
+        processed_data[patient_folder]['ecg']={"signals":ecg_signals, "info":ecg_info}
+
+    #GSR
+    signal3=patient_data[patient_folder]['gsr'] 
+    if signal3 is not None:
+        gsr_signals, gsr_info=nk.eda_process(signal3["Conductance"],
+                                             sampling_rate=100)
+        processed_data[patient_folder]['gsr']={"signals": gsr_signals, "info": gsr_info}
+
+
+
+
 
